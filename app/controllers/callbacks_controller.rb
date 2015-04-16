425e326e08c5da04ec50c810a3da44903d6633a0
Mike Virata-Stone <mike@virata-stone.com>
2015-04-16 02:36:42 -0700

    Remember for a long time... why not?

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,9 @@
 class CallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   def github
-    @user = User.from_auth request.env["omniauth.auth"]
-    sign_in_and_redirect @user
+    user = User.from_auth request.env["omniauth.auth"]
+    sign_in_and_redirect user
+    remember_me user
   end
 end
